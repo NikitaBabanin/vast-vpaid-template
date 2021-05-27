34f9fcf7a02447576e848a43e93d206ced1f7ef7
Niks0n <nikitab@youhunted.com>
2021-05-27 14:23:03 +0300

    up

--- client/js/vast.js
+++ client/js/vast.js
@@ -5,21 +5,57 @@ function chackVastUrl() {
   let vastUrl = checkVastUrlData.getAttribute("vastTag");
   console.log(vastUrl);
   if (vastUrl) {
-    parseVastXml();
+    parseVastXml(vastUrl);
   }
 }
 
-function parseVastXml() {
+function parseVastXml(vastUrl) {
   try {
-    const vastUrl = {
-      vastXml:
-        "https://kme-vast-sample.s3.us-east-2.amazonaws.com/vast-v4.0-inline-interactive.xml",
+    const url = {
+      vastXml: vastUrl,
     };
 
     axios
-      .post("https://warm-fortress-44560.herokuapp.com/api/parse/", vastUrl)
-      .then((res) => console.log("fetch ", res));
+      .post("https://warm-fortress-44560.herokuapp.com/api/parse/", url)
+      .then((res) => dataPreparation(res.data));
   } catch (err) {
     console.log("error", err.message);
   }
 }
+
+// Search for the root node (Wrapper or InLine)
+function dataPreparation(data) {
+  let vastRoot = data.vastJson.elements[0].elements;
+
+  //get Ad tag
+  let adTag = vastRoot.filter((item) => item.name === "Ad")[0];
+
+  //get wrapper or InLine.
+  //It is assumed that there will be only one nested element
+  let typeOfAd = adTag.elements[0];
+
+  //check wrapper or InLine
+  if (typeOfAd.name === "InLine") {
+    inLineNode(typeOfAd.elements);
+  } else if (typeOfAd.name === "Wrapper") {
+    wrapperNode();
+  } else {
+    return;
+  }
+}
+
+function inLineNode(inlineElements) {
+  console.log("inline ", inlineElements);
+
+  inlineElements.forEach((element) => {
+    if (element.name === "Creatives") {
+      parseCreatives(element);
+    }
+  });
+}
+
+function wrapperNode() {
+  console.log("wrapper");
+}
+
+function parseCreatives() {}
